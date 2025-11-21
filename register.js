4f2e3495fcae7970e6c98406fce050c61afc5d5e
Mostafa Abd Elhamied Ismael <155177290+MostafaAbdElhamied@users.noreply.github.com>
2025-11-21 21:50:56 +0200

    Adding the API Call and handling

--- register.js
+++ register.js
@@ -1,62 +1,75 @@
-// Get the form
+
 const form = document.querySelector("form");
 
-// Handle submit
-form.addEventListener("submit", function(e) {
-    // Get all field values
+
+form.addEventListener("submit", async function(e) {
+    e.preventDefault(); //stopping form from submitting by default.
     const username = document.getElementById("username").value.trim();
     const email = document.getElementById("email").value.trim();
     const password = document.getElementById("password").value.trim();
     
-    // For number fields, get them as numbers
     const weight = parseFloat(document.getElementById("weight").value);
     const height = parseFloat(document.getElementById("height").value);
     const age = parseInt(document.getElementById("age").value);
 
-    // --- Validation Checks ---
 
-    // Check username
     if (username.length < 3) {
         alert("Username must be at least 3 letters");
-        e.preventDefault(); // Stop submit
+        e.preventDefault(); 
         return;
     }
 
-    // Check email
     if (!email.includes("@") || !email.includes(".")) {
         alert("Please enter a valid email");
         e.preventDefault();
         return;
     }
 
-    // Check password
     if (password.length < 6) {
         alert("Password must be at least 6 characters");
         e.preventDefault();
         return;
     }
 
-    // Check weight
     if (isNaN(weight) || weight <= 0) {
         alert("Please enter a valid weight (must be > 0)");
         e.preventDefault();
         return;
     }
 
-    // Check height
     if (isNaN(height) || height <= 0) {
         alert("Please enter a valid height (must be > 0)");
         e.preventDefault();
         return;
     }
 
-    // Check age
     if (isNaN(age) || age <= 1) {
         alert("Please enter a valid age (must be > 1)");
         e.preventDefault();
         return;
     }
-
-    // If all checks pass:
-    alert("Registration Successful!");
+    console.log("test1");
+    try{
+        const respone = await fetch("https://192.168.1.101:7019/api/Authentication/register", {
+            method:"POST",
+            headers: {"Content-Type":"application/json"},
+            body: JSON.stringify({
+                username:username,
+                email:email,
+                password:password,
+                weight:weight,
+                height:height,
+                age:age
+            })
+        });
+        console.log("test2");
+        if(respone.ok){
+            alert("Registeration success! ");
+            window.location.href = "login Page.html"
+        }else{
+            alert("Invalid credentials! ");
+        }
+    }catch(error){
+            console.log(error);
+        }
 });
